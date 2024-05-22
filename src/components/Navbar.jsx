f03d3abcc571017f44cb184cc68b4fc2050e05dc
bschettinodev <biel.schettino@gmail.com>
2024-05-22 02:55:07 -0300

    styling component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,12 @@
 import React from "react"
+import ReactLogo from '../images/react-logo.png'
 
 export default function Navbar() {
-    return <h1>Navbar component</h1>
+    return (
+        <nav className="navbar">
+            <img src={ReactLogo} alt="React Logo" className="navbar--logo" />
+            <h1 className="navbar--title">ReactFacts</h1>
+            <h4 className="navbar--subtitle">React Course - Project 1</h4>
+        </nav>
+    )
 }
